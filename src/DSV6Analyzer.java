0d43c60b6de93ddbf05569525b50c56fb0600d62
DennisWeiss <weiss.dennis98@yahoo.de>
2017-10-24 23:12:56 +0200

    changed to SQLite

--- src/DSV6Analyzer.java
+++ src/DSV6Analyzer.java
@@ -1,3 +1,4 @@
+import javax.print.DocFlavor;
 import java.io.*;
 import java.sql.*;
 import java.text.ParseException;
@@ -8,7 +9,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class DSV6Analyzer {
-    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+    Connection connection = DriverManager.getConnection("jdbc:sqlite:SwimDB.db", "root", "");
     Statement statement = connection.createStatement();
 
     String veranstaltungBezeichnung = "";
@@ -26,8 +27,12 @@ public class DSV6Analyzer {
 
         try {
 
+            //statement.execute("CREATE DATABASE test;");
+
+            //statement.execute("USE test;");
+
             statement.execute("CREATE TABLE Veranstaltung(" +
-                    "VID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "VID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "Bezeichnung VARCHAR(200) NOT NULL, " +
                     "Ort VARCHAR(200) NOT NULL, " +
                     "Bahnlange VARCHAR(40) NOT NULL, " +
@@ -36,12 +41,12 @@ public class DSV6Analyzer {
             );
 
             statement.execute("CREATE TABLE Veranstalter(" +
-                    "VerID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "VerID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "Name VARCHAR(100) UNIQUE NOT NULL);"
             );
 
             statement.execute("CREATE TABLE Ausrichter(" +
-                    "AID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "AID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "NameAusrichter VARCHAR(200) NOT NULL, " +
                     "NameAnsprechpartner VARCHAR(200) NOT NULL, " +
                     "Strasse VARCHAR(200), " +
@@ -67,7 +72,7 @@ public class DSV6Analyzer {
             );
 
             statement.execute("CREATE TABLE Kampfrichter(" +
-                    "KID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "KID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "Nachname VARCHAR(60) NOT NULL, " +
                     "Vorname VARCHAR(60) NOT NULL, " +
                     "VID INTEGER REFERENCES Verein);"
@@ -93,11 +98,12 @@ public class DSV6Analyzer {
                     "Wertungstyp CHAR(2) NOT NULL, " +
                     "JahrgangVon INTEGER NOT NULL, " +
                     "JahrgangBis INTEGER, " +
+                    "Geschlecht CHAR(1), " +
                     "Wertungsname VARCHAR(60) NOT NULL);"
             );
 
             statement.execute("CREATE TABLE Verein(" +
-                    "VID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "VID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "Bezeichnung VARCHAR(100) UNIQUE NOT NULL, " +
                     "Kennzahl INTEGER, " +
                     "Landesschwimmverband INTEGER DEFAULT NULL, " +
@@ -105,7 +111,7 @@ public class DSV6Analyzer {
             );
 
             statement.execute("CREATE TABLE Ergebnis(" +
-                    "EID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "EID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "WertungsID INTEGER REFERENCES Wertung, " +
                     "Platz INTEGER NOT NULL, " +
                     "GrundDerNichtwertung VARCHAR(100), " +
@@ -123,7 +129,7 @@ public class DSV6Analyzer {
             );
 
             statement.execute("CREATE TABLE Zwischenzeit(" +
-                    "ZID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "ZID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "VeranstaltungsID INTEGER REFERENCES VeranstaltungSchwimmer, " +
                     "Wettkampfnummer INTEGER REFERENCES Wettkampf, " +
                     "Distanz INTEGER NOT NULL, " +
@@ -131,7 +137,7 @@ public class DSV6Analyzer {
             );
 
             statement.execute("CREATE TABLE Reaktion(" +
-                    "RID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "RID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "VeranstaltungsID INTEGER REFERENCES VeranstaltungSchwimmer, " +
                     "Wettkampfnummer INTEGER REFERENCES Wettkampf, " +
                     "Art CHAR(1), " +
@@ -139,7 +145,7 @@ public class DSV6Analyzer {
             );
 
             statement.execute("CREATE TABLE Schwimmer(" +
-                    "SID INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+                    "SID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "Nachname VARCHAR(60) NOT NULL, " +
                     "Vorname VARCHAR(60) NOT NULL, " +
                     "DSVID INTEGER NOT NULL, " +
@@ -258,6 +264,99 @@ public class DSV6Analyzer {
                         "' AND Vorname = '" + vorname + "' AND VID = (SELECT VID FROM Verein WHERE Bezeichnung = '" +
                         verein + "')));"
                 );
+            } else if (line.substring(0, 10).equals("WETTKAMPF:")) {
+                int wettkampfnummer = Integer.parseInt(stringsSeparatedBySemicolons(line, 11).get(0));
+                char wettkampfart = stringsSeparatedBySemicolons(line, 11).get(1).charAt(0);
+                int abschnittsnummer = Integer.parseInt(stringsSeparatedBySemicolons(line, 11).get(2));
+                int starter = Integer.parseInt(stringsSeparatedBySemicolons(line, 11).get(3));
+                int strecke = Integer.parseInt(stringsSeparatedBySemicolons(line, 11).get(4));
+                char technik = stringsSeparatedBySemicolons(line, 11).get(5).charAt(0);
+                String ausubung = stringsSeparatedBySemicolons(line, 11).get(6);
+                char geschlecht = stringsSeparatedBySemicolons(line, 11).get(7).charAt(0);
+                String bestenliste = stringsSeparatedBySemicolons(line, 11).get(8);
+                if (!stringsSeparatedBySemicolons(line, 11).get(9).equals("") && !stringsSeparatedBySemicolons(line, 11).get(10).equals("")) {
+                    int qualiwettkampfnummer = Integer.parseInt(stringsSeparatedBySemicolons(line, 11).get(9));
+                    char qualiwettkampfart = stringsSeparatedBySemicolons(line, 11).get(10).charAt(0);
+                    System.out.println("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste, Qualifikationswettkampfnummer, Qualifikationswettkampfart) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "', " +
+                            qualiwettkampfnummer + ", '" + qualiwettkampfart + "');");
+                    statement.execute("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste, Qualifikationswettkampfnummer, Qualifikationswettkampfart) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "', " +
+                            qualiwettkampfnummer + ", '" + qualiwettkampfart + "');"
+                    );
+                } else if (stringsSeparatedBySemicolons(line, 11).get(9).equals("") && !stringsSeparatedBySemicolons(line, 11).get(10).equals("")) {
+                    char qualiwettkampfart = stringsSeparatedBySemicolons(line, 11).get(10).charAt(0);
+                    System.out.println("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste, Qualifikationswettkampfart) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "', '" +
+                            qualiwettkampfart + "');");
+                    statement.execute("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste, Qualifikationswettkampfart) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "', '" +
+                            qualiwettkampfart + "');"
+                    );
+                } else if (!stringsSeparatedBySemicolons(line, 11).get(9).equals("") && stringsSeparatedBySemicolons(line, 11).get(10).equals("")) {
+                    int qualiwettkampfnummer = Integer.parseInt(stringsSeparatedBySemicolons(line, 11).get(9));
+                    System.out.println("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste, Qualifikationswettkampfnummer) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "', " +
+                            qualiwettkampfnummer + ");");
+                    statement.execute("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste, Qualifikationswettkampfnummer) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "', " +
+                            qualiwettkampfnummer + ");"
+                    );
+                } else if (stringsSeparatedBySemicolons(line, 11).get(9).equals("") && stringsSeparatedBySemicolons(line, 11).get(10).equals("")) {
+                    System.out.println("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "');");
+                    statement.execute("INSERT INTO Wettkampf(Wettkampfnummer, Wettkampfart, Abschnittsnummer, Starter, Einzelstrecke, " +
+                            "Technik, Ausubung, Geschlecht, Bestenliste) VALUES(" +
+                            wettkampfnummer + ", '" + wettkampfart + "', " + abschnittsnummer + ", " + starter + ", " +
+                            strecke + ", '" + technik + "', '" + ausubung + "', '" + geschlecht + "', '" + bestenliste + "');"
+                    );
+                }
+            } else if (line.substring(0, 8).equals("WERTUNG:")) {
+                int wettkampfnummer = Integer.parseInt(stringsSeparatedBySemicolons(line, 9).get(0));
+                char wettkampfart = stringsSeparatedBySemicolons(line, 9).get(1).charAt(0);
+                int wertungsid = Integer.parseInt(stringsSeparatedBySemicolons(line, 9).get(2));
+                String wertungstyp = stringsSeparatedBySemicolons(line, 9).get(3);
+                int jahrgangVon = Integer.parseInt(stringsSeparatedBySemicolons(line, 9).get(4));
+                String wertungsname = stringsSeparatedBySemicolons(line, 9).get(7);
+                if (!stringsSeparatedBySemicolons(line, 9).get(5).equals("") && !stringsSeparatedBySemicolons(line, 9).get(6).equals("")) {
+                    int jahrgangBis = Integer.parseInt(stringsSeparatedBySemicolons(line, 9).get(5));
+                    char geschlecht = stringsSeparatedBySemicolons(line, 9).get(6).charAt(0);
+                    statement.execute("INSERT INTO Wertung(WertungsID, Wettkampfnummer, Wertungstyp, JahrgangVon, JahrgangBis, Geschlecht, Wertungsname) VALUES(" +
+                            wertungsid + ", " + wettkampfnummer + ", '" + wertungstyp + "', " + jahrgangVon + ", " +
+                            jahrgangBis + ", '" + geschlecht + "', '" + wertungsname + "');"
+                    );
+                } else if (stringsSeparatedBySemicolons(line, 9).get(5).equals("") && !stringsSeparatedBySemicolons(line, 9).get(6).equals("")) {
+                    char geschlecht = stringsSeparatedBySemicolons(line, 9).get(6).charAt(0);
+                    statement.execute("INSERT INTO Wertung(WertungsID, Wettkampfnummer, Wertungstyp, JahrgangVon, Geschlecht, Wertungsname) VALUES(" +
+                            wertungsid + ", " + wettkampfnummer + ", '" + wertungstyp + "', " + jahrgangVon + ", '" +
+                            geschlecht + "', '" + wertungsname + "');"
+                    );
+                } else if (!stringsSeparatedBySemicolons(line, 9).get(5).equals("") && stringsSeparatedBySemicolons(line, 9).get(6).equals("")) {
+                    int jahrgangBis = Integer.parseInt(stringsSeparatedBySemicolons(line, 9).get(5));
+                    statement.execute("INSERT INTO Wertung(WertungsID, Wettkampfnummer, Wertungstyp, JahrgangVon, JahrgangBis, Wertungsname) VALUES(" +
+                            wertungsid + ", " + wettkampfnummer + ", '" + wertungstyp + "', " + jahrgangVon + ", " +
+                            jahrgangBis + ", '" + wertungsname + "');"
+                    );
+                } else if (stringsSeparatedBySemicolons(line, 9).get(5).equals("") && stringsSeparatedBySemicolons(line, 9).get(6).equals("")) {
+                    statement.execute("INSERT INTO Wertung(WertungsID, Wettkampfnummer, Wertungstyp, JahrgangVon, Wertungsname) VALUES(" +
+                            wertungsid + ", " + wettkampfnummer + ", '" + wertungstyp + "', " + jahrgangVon + ", '" +
+                            wertungsname + "');"
+                    );
+                }
             }
         } catch (StringIndexOutOfBoundsException e) {
 
